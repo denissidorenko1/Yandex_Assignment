a725c9bed5c40c9f5951b413b6c5bbed7befb2f8
Denis <fpv.airplane.barnaul@gmail.com>
2024-06-20 18:45:21 +0300

    Initial Commit

---
+++ Yandex_Assignment/Yandex_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yandex_AssignmentApp.swift
+//  Yandex_Assignment
+//
+//  Created by Denis on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Yandex_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
